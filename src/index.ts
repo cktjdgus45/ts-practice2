adcb82530b0ae64086f3d67aa96f0376fdee9317
pooh <cktjdgus45@naver.com>
2022-05-05 12:59:42 +0900

    contional type

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,12 @@
-//mapped Type
+//Condition Type
 
-//[1,2].map(item=>item*item); // [1,4]
-
-type Video = {
-    title: string;
-    author: string;
-}
-
-type Optional<T> = {
-    [P in keyof T]?: T[P] // for... in
-}
-
-type ReadOnly<T> = {
-    readonly [P in keyof T]: T[P];
-}
-
-type VideoOptional = Optional<Video>;
-
-const videoOp: VideoOptional = {
-    title: 'autor',
-    author: 'author'
-}
-
-type Animal = {
-    name: string;
-    age: number;
-}
-
-const animal: Optional<Animal> = {
-    name: 'dog'
-}
+type Check<T> = T extends string
+    ? string
+    : T extends number
+    ? 'number'
+    : T extends boolean
+    ? 'boolean'
+    : T extends Function
+    ? 'funcion'
+    : 'object';
+type Type = Check<string>; // boolean;
