c76e7ee9ee8c8ef31171ba5279583cec8b0a7df8
pooh <cktjdgus45@naver.com>
2022-05-09 18:30:06 +0900

    get videoId by regex

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -8,9 +8,18 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
                 </section>`)
 
         const iframeElement = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
-        iframeElement.src = url;
+        iframeElement.src = this.convertToEmbeddedURL(url);
 
         const titleElement = this.element.querySelector('.video__title')! as HTMLHeadElement;
         titleElement.textContent = title;
+    }
+    private convertToEmbeddedURL(url: string): string {
+        const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+        const match = url.match(regExp);
+        const videoId = match ? match[1] || match[2] : undefined;
+        if (videoId) {
+            return `https://www.youtube.com/embed/${videoId}`;
+        }
+        return url;
     }
 }
