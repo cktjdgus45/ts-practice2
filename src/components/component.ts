e0cff430ae770c0fe71319d6e5b9a694d0dadb2a
pooh <cktjdgus45@naver.com>
2022-05-10 18:47:47 +0900

    implements delete button

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,5 +1,6 @@
 export interface Component {
     attachTo(element: HTMLElement, position?: InsertPosition): void;
+    removeFrom(parent: HTMLElement): void;
 }
 
 export class BaseComponent<T extends HTMLElement> implements Component {
@@ -11,5 +12,9 @@ export class BaseComponent<T extends HTMLElement> implements Component {
     }
     attachTo(element: HTMLElement, position: InsertPosition = 'afterbegin') {
         element.insertAdjacentElement(position, this.element);
+    }
+    removeFrom(parent: HTMLElement) {
+
+        parent.removeChild(this.element);
     }
 }
