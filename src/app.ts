c65922841a8a215616f5532119d0a935d10facce
pooh <cktjdgus45@naver.com>
2022-05-10 18:18:06 +0900

    add page Item Container

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,25 @@
+import { Component } from './components/component.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent } from './components/page/page.js';
+import { PageComponent, Composable } from './components/page/page.js';
 
 class App {
-    private readonly page: PageComponent
+    private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        image.attachTo(appRoot, 'beforeend')
+        this.page.addChild(image);
         const note = new NoteComponent('Note Title', 'Note Body');
-        note.attachTo(appRoot, 'beforeend');
+        this.page.addChild(note);
 
         const todo = new TodoComponent('Todo Title', 'Todo item');
-        todo.attachTo(appRoot, 'beforeend');
+        this.page.addChild(todo);
 
         const video = new VideoComponent('Video Title', "https://www.youtube.com/embed/DPmX85Tj8fY");
-        video.attachTo(appRoot, 'beforeend');
+        this.page.addChild(video);
     }
 }
 
